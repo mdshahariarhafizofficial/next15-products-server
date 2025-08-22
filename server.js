657f80850bcf992313240c9fe0757241edf38d0a
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-08-23 00:52:48 +0600

    deploy error solved

--- server.js
+++ server.js
@@ -96,4 +96,4 @@ app.delete("/api/products/:id", async (req, res) => {
 // =========================
 // Start server
 // =========================
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
